d40c1b41a80686fc05940302e2b3f7f5a29567ba
Nikita Kovalevskiy <koval701@gmail.com>
2022-01-29 17:46:20 +0700

    update to deploy

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -147,7 +147,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/api/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
